0374c55ce93be0cf29034191a531748be71eaded
zayim <zayim92@gmail.com>
2015-03-21 13:16:46 +0100

    ZAD-10 Done.

--- zad10/zad10.cpp
+++ zad10/zad10.cpp
@@ -2,8 +2,10 @@
 #include <stack>
 #include <queue>
 #include <sstream>
+#include <fstream>
+
 using namespace std;
-int stepen2(int n)
+int log2(int n)
 {
     int k=0;
     while (n%2==0)
@@ -13,10 +15,10 @@ int stepen2(int n)
     }
     return k;
 }
-int dvana(int n)
+int power2(int n)
 {
     int k=1;
-    for (int i=1; i<=n; i++)
+    for (int i=0; i<n; i++)
         k*=2;
     return k;
 }
@@ -26,47 +28,58 @@ string str(int k)
     ss << k;
     return ss.str();
 }
-void f(int n, stack<string> &s)
+void g(int n, stack<string> &s)
 {
     if (n==1) return;
 
-    int k = stepen2(n);
-    int m = n/dvana(k);
+    int k = log2(n);
+    int m = n/power2(k);
 
     if (k!=0)
     {
         s.push(string("SHL A,") + str(k));
-        f(m,s);
+        g(m, s);
         return;
     }
 
     if ((n+1)%4==0)
     {
         s.push(string("SUB A,B"));
-        f(n+1,s);
+        g(n+1, s);
         return;
     }
 
     s.push(string("ADD A,B"));
-    f(n-1,s);
-
+    g(n-1, s);
 }
-void g(int n)
+stack<string> f(int n)
 {
-
     stack<string> s;
-    f(n,s);
-    s.push(string("MOV A,B"));
+    g(n, s);
+    s.push(string("MOV B,A"));
 
+    return s;
+}
+int main(int argc, char *argv[])
+{
+    int n;
+
+    char* input_path = "M_ulaz.dat";
+    char* output_path = "M_izlaz.dat";
+
+    if (argc > 1) input_path = argv[1];
+    if (argc > 2) output_path = argv[2];
+
+    ifstream input(input_path);
+    ofstream output(output_path);
+
+    input >> n;
+    stack<string> s = f(n);
     while(!s.empty())
     {
-        cout << s.top() << endl;
+        output << s.top() << endl;
         s.pop();
     }
-}
-int main()
-{
-    g(417);
 }
 
 
